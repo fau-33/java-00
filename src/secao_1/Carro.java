5a875d27da18afec86768236f7443bcf911fe02b
Flávio Félix <fau_felix30@hotmail.com>
2025-04-11 14:42:59 -0300

    Start POO

---
+++ src/secao_1/Carro.java
@@ -0,0 +1,17 @@
+public class Carro {
+  // Atributos e propriedades
+  String marca;
+  String modelo;
+  int ano;
+  
+  // Métodos
+  void acelerar() {
+    System.out.println("Acelerando");
+  }
+
+  void info() {
+    System.out.println("Marca: " + marca);
+    System.out.println("Modelo: " + modelo);
+    System.out.println("Ano: " + ano);
+  }
+}
